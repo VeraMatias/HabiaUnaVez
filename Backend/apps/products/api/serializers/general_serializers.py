1542e2263bff2c634c886c18f15d209cea4bfc53
VeraMatias <veramatias93@hotmail.com>
2023-11-06 16:45:30 -0300

    Agregado de product_quantity a SupplierSerializer

--- Backend/apps/products/api/serializers/general_serializers.py
+++ Backend/apps/products/api/serializers/general_serializers.py
@@ -17,4 +17,13 @@ class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
         exclude = ('state','created_date','modified_date','deleted_date',)
+
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'name': instance.name,
+            'product_quantity': len(Product.objects.filter(supplier = instance.id)),
+            'url': instance.url,
+            'armhole': instance.armhole
+        }
     
